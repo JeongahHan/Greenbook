d24dc401cb370ab5be47931315f9be72746af37c
khsgsong <khsgsong@gmail.com>
2023-08-17 19:47:12 +0900

    Initial commit

---
+++ src/main/java/kr/or/bo/GreenbookProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.or.bo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenbookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenbookProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/or/bo/GreenbookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.bo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
