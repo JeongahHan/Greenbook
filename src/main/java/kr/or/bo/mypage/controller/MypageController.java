e7e563b8cc1f2626906452cc9856359b64ecf579
JaeWook <142170487+pondue@users.noreply.github.com>
2023-08-18 15:22:29 +0900

    푸시하자~

--- src/main/java/kr/or/bo/mypage/controller/MypageController.java
+++ src/main/java/kr/or/bo/mypage/controller/MypageController.java
@@ -2,6 +2,7 @@ package kr.or.bo.mypage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.bo.mypage.model.service.MypageService;
@@ -11,5 +12,11 @@ import kr.or.bo.mypage.model.service.MypageService;
 public class MypageController {
 	@Autowired
 	private MypageService mypageService;
+	
+	@GetMapping(value = "/memberUpdateFrm")
+	public String memberUpdateFrm () {
+		
+		return "mypage//memberUpdateFrm";
+	}
 	
 }
