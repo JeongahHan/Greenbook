984c3824748d2810509790ad423f70144f2e6efa
Leeeyuna <142005889+Leeeyuna@users.noreply.github.com>
2023-08-19 22:18:57 +0900

    관라자 커밋

--- src/main/java/kr/or/bo/member/model/service/MemberService.java
+++ src/main/java/kr/or/bo/member/model/service/MemberService.java
@@ -36,13 +36,13 @@ public class MemberService {
 		while(sT1.hasMoreTokens()) {
 			int memberNo = Integer.parseInt(sT1.nextToken());
 			int memberLevel = Integer.parseInt(sT2.nextToken());
-			int changeResult = memberDao.changeLevel(memberNo, memberLevel);	//레벨변경 재활용
-			if(changeResult == 0) {	//실패
+			int changeResult = memberDao.changeLevel(memberNo, memberLevel);	
+			if(changeResult == 0) {	
 				result = false;
-				break;	//한 번이라도 실패가 일어나면 더 할 필요가 없으니까 내보냄
+				break;	
 			}
 		}
-		return result;	//한 번이라도 실패 : false, 전부 성공하면 true
+		return result;
 	}
 	public Member selectOneMember(String checkId) {
 		Member m = memberDao.selectOneMember(checkId);
