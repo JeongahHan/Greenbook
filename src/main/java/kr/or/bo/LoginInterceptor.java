72a9ad85668346065d63947a6305b3ddf1524472
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-08-21 16:49:27 +0900

    interceptor 추가

---
+++ src/main/java/kr/or/bo/LoginInterceptor.java
@@ -0,0 +1,31 @@
+package kr.or.bo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.or.bo.member.model.vo.Member;
+
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		Member m = (Member)session.getAttribute("m");
+		System.out.println(m);
+		if(m == null) {
+			response.sendRedirect("/member/loginMsg");
+			return false;
+		}else {
+			return true;
+		}
+	}
+
+	
+}
