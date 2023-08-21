b01b2dafa26ad33da10fc0de8ee63b8e8a9af067
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-08-21 14:56:28 +0900

    product

--- src/main/java/kr/or/bo/product/controller/ProductController.java
+++ src/main/java/kr/or/bo/product/controller/ProductController.java
@@ -3,11 +3,15 @@ package kr.or.bo.product.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.bo.FileUtil;
 import kr.or.bo.product.model.service.ProductService;
+import kr.or.bo.product.model.vo.Product;
 
 @Controller
 @RequestMapping(value="/product")
@@ -30,6 +34,15 @@ public class ProductController {
 	@GetMapping(value="/writeFrm")
 	public String writeFrm() {
 		return "product/writeFrm";
+	}
+	
+	@PostMapping(value="/wirte")
+	public String write(Product p, MultipartFile imageFile, Model model) {
+		String savepath = root+"product/";
+		String filepath = fileUtil.getFilepath(savepath, imageFile.getOriginalFilename());
+		
+		return root;
+		
 	}
 	
 }
