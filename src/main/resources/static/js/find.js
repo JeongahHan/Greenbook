1a048792f98675fd9d0eef9f44778a505293e9fd
yuna <142187063+jeongahChae@users.noreply.github.com>
2023-08-22 17:10:34 +0900

    관리자 기능수정

---
+++ src/main/resources/static/js/find.js
@@ -0,0 +1,21 @@
+$(function () {
+  $(document).on("click", ".modal-open-btn", function () {
+    $($(this).attr("target")).css("display", "flex").css("", "flex");
+  });
+  $(document).on("click", ".modal-close", function () {
+    $(this).parents(".modal-wrap").parent().css("display", "none");
+  });  
+});
+function alertFunc(icon){
+	swal({
+        title: '제목',
+        text: '내용내용내용',
+        icon: icon
+      });
+      //이후 페이지 이동 시 추가
+      /*
+      .then(function(){
+    	 window.location = "${loc}"; 
+      });
+      */
+}
