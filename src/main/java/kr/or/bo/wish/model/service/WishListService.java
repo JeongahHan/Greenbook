fb065396ececaf0fab391ae5e7de9ff50c8f10fa
khsgsong <142006448+khsgsong@users.noreply.github.com>
2023-08-27 16:07:41 +0900

    관심상품 등록, 해제 기능 추가

--- src/main/java/kr/or/bo/wish/model/service/WishListService.java
+++ src/main/java/kr/or/bo/wish/model/service/WishListService.java
@@ -2,6 +2,7 @@ package kr.or.bo.wish.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.or.bo.wish.model.dao.WishListDao;
 
@@ -9,4 +10,16 @@ import kr.or.bo.wish.model.dao.WishListDao;
 public class WishListService {
 	@Autowired
 	private WishListDao wishListDao;
+	
+	@Transactional
+	public int insertWish(int productBoardNo, String memberId) {
+		int result = wishListDao.insertWish(productBoardNo, memberId);
+		return result;
+	}
+	
+	@Transactional
+	public int deleteWish(int productBoardNo, String memberId) {
+		int result = wishListDao.deleteWish(productBoardNo, memberId);
+		return result;
+	}
 }
