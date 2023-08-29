69555ae2da3bf127d785b5936c5335677f145304
JeongahHan <142190043+JeongahHan@users.noreply.github.com>
2023-08-29 16:05:23 +0900

    통합검색기능구현

--- src/main/java/kr/or/bo/board/vo/MainSearchList.java
+++ src/main/java/kr/or/bo/board/vo/MainSearchList.java
@@ -9,28 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Board {
+public class MainSearchList {
 	private int boardNo;
 	private String boardTitle;
 	private String boardContent;
 	private String boardWriter;
 	private String boardRegDate;
 	private int boardReadCount;
-	
 	private String boardType;
 	
-	private List fileList; //리스트 타입 객체 선언 
-	
-	
-	//검색필터
-	private String type; //검색타입
-	private String keyword; //검색내용
-	
-
-	
-	
-	public String getBoardContentBr() {
-		//컨텐츠 작성자가 엔터를 입력하면 > 그 엔터를 br로 바꿔주기
-		return boardContent.replaceAll("\r\n","<br>");
-	}
 }
