5eb1ce3079cc75ff507ac5d1c4cdd01e7d761c4c
JaeWook <142170487+pondue@users.noreply.github.com>
2023-08-29 12:14:39 +0900

    멤버업데이트 유효성 검사

--- src/main/resources/static/js/memberUpdate.js
+++ src/main/resources/static/js/memberUpdate.js
@@ -3,7 +3,6 @@ console.log("멤버업데이트 연결확인");
 const checkArr = [false,false,false,false];
 
 //비밀번호 유효성 검사
-
 $("#memberPw").on("change",function(){
     //비밀번호 : 영어 소문자+대문자+숫자로 8~12글자
     const pwReg = /^[a-zA-Z0-9]{8,12}$/;
@@ -36,7 +35,10 @@ $("#memberPw").on("change",function(){
 
 //새 비밀번호 확인 유효성 검사
 $("#memberPwRe").on("change",function(){
-    const pwValue = $("#memberPw").val();
+    pwDupCHECK();
+});
+function pwDupCHECK(){
+	const pwValue = $("#memberPw").val();
     const pwReValue = $("#memberPwRe").val();
     if($("#memberPwRe").val() != ""){
 	    if(pwValue == pwReValue){
@@ -54,4 +56,5 @@ $("#memberPwRe").on("change",function(){
     	$("#checkPwRe").text("");
     	$("#memberPwRe").css("border","1px solid #ccc");
     }
-});
+
+}
