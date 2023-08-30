b9b5f1de60d9eca0a5c0c2d6e093c68b8145e2d9
JaeWook <142170487+pondue@users.noreply.github.com>
2023-08-30 20:03:19 +0900

    넘퍼페이지 임시로 5

--- src/main/java/kr/or/bo/mypage/model/service/MypageService.java
+++ src/main/java/kr/or/bo/mypage/model/service/MypageService.java
@@ -118,7 +118,7 @@ public class MypageService {
 		//여기서 페이지 나비 만들기
 		// 1. 한페이지당 게시물 수 지정 -> 10개
 
-		int numPerPage = 10;
+		int numPerPage = 5;
 		int end = reqPage * numPerPage;
 		int start = end - numPerPage + 1;
 		List mySellBookList = mypageDao.selectMySellBook(m.getMemberId(), start, end);
@@ -570,7 +570,7 @@ public class MypageService {
 	@Transactional
 	public MypageListData selectByRequestList(String memberId, int reqPage) {
 		
-		int numPerPage = 10;
+		int numPerPage = 5;
 		int end = reqPage * numPerPage;
 		int start = end - numPerPage + 1;
 		
