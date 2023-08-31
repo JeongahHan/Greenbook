62dfb8bfcb73a845b30e53d18ca888f47fc6974a
JeongahHan <142190043+JeongahHan@users.noreply.github.com>
2023-08-31 15:35:22 +0900

    수정

--- src/main/java/kr/or/bo/board/controller/BoardController.java
+++ src/main/java/kr/or/bo/board/controller/BoardController.java
@@ -114,53 +114,57 @@ public class BoardController {
 		
 		//null 일 시 , "/"
 		if(b.getBoardContent().length()==0) {
-			b.setBoardContent("/");
-		}
-		
-		ArrayList<BoardFile> fileList = null; //fileList == null 
-		//첨부파일 목록을 저장할 리스트
-		if(!upfile[0].isEmpty()) {
-			//파일이 없어도 1 (input 때문에) ,  하나여도 1 . 없는건 "첫번째 자리가 비어있으면 이게 진짜 파일이 없는거임"
-			fileList = new ArrayList<BoardFile>(); //첨부파일 있는 경우에만 리스트 객체를 생성
-			String savepath = root+"boardFile/";  //업로드한 경로
-			//파일 업로드는 배열을 반복해줘야함
-			for(MultipartFile file : upfile) {
-				String filename = file.getOriginalFilename();//사용자가 실제로 올린 파일이름
-				
-				String filepath = fileUtil.getFilepath(savepath, filename); //중복파일명 체크 : 파일경로주고 파일명 줘서 이거보고 체크해서 안겹치게만듬
-				
-				File uploadFile = new File(savepath+filepath); //실제 폴더에 파일을 업로드
-				try {
-					file.transferTo(uploadFile);
-				} catch (IllegalStateException | IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				//첨부파일 업로드 끝! 
-				
-				//오류 parent key not found: 부모테이블에 내가 원하는 키가 없다는 뜻 
-				BoardFile bf = new BoardFile(); //현재 noticeno > 0임 >> 그래서 0값 넘겨줘서 해당 오류 남
-				bf.setFilename(filename);
-				bf.setFilepath(filepath);
-				fileList.add(bf);
-			}
-		}
-		
-		int result = boardService.insertBoard(b,fileList);
-		//성공케이스 
-		//1.파일이 없으면 result -> 1
-		//2. 파일이 있는 경우 result -> 파일개수 +1
-		
-		
-		if((fileList == null && result==1) || (fileList !=null && result==(fileList.size()+1))) { //성공 조건 
-			model.addAttribute("title","게시판 작성 성공");
-			model.addAttribute("msg","게시판 작성되었습니다");
+			model.addAttribute("title","게시글을 입력해주세요.");
+			model.addAttribute("icon","warning");
+			model.addAttribute("loc","/board/writeFrm");
 		}else {
-			model.addAttribute("title","게시판 작성 실패");
-			model.addAttribute("msg","에러찾으세요...");
+			ArrayList<BoardFile> fileList = null; //fileList == null 
+			//첨부파일 목록을 저장할 리스트
+			if(!upfile[0].isEmpty()) {
+				//파일이 없어도 1 (input 때문에) ,  하나여도 1 . 없는건 "첫번째 자리가 비어있으면 이게 진짜 파일이 없는거임"
+				fileList = new ArrayList<BoardFile>(); //첨부파일 있는 경우에만 리스트 객체를 생성
+				String savepath = root+"boardFile/";  //업로드한 경로
+				//파일 업로드는 배열을 반복해줘야함
+				for(MultipartFile file : upfile) {
+					String filename = file.getOriginalFilename();//사용자가 실제로 올린 파일이름
+					
+					String filepath = fileUtil.getFilepath(savepath, filename); //중복파일명 체크 : 파일경로주고 파일명 줘서 이거보고 체크해서 안겹치게만듬
+					
+					File uploadFile = new File(savepath+filepath); //실제 폴더에 파일을 업로드
+					try {
+						file.transferTo(uploadFile);
+					} catch (IllegalStateException | IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					//첨부파일 업로드 끝! 
+					
+					//오류 parent key not found: 부모테이블에 내가 원하는 키가 없다는 뜻 
+					BoardFile bf = new BoardFile(); //현재 noticeno > 0임 >> 그래서 0값 넘겨줘서 해당 오류 남
+					bf.setFilename(filename);
+					bf.setFilepath(filepath);
+					fileList.add(bf);
+				}
+			}
+			
+			int result = boardService.insertBoard(b,fileList);
+			//성공케이스 
+			//1.파일이 없으면 result -> 1
+			//2. 파일이 있는 경우 result -> 파일개수 +1
+			
+			
+			if((fileList == null && result==1) || (fileList !=null && result==(fileList.size()+1))) { //성공 조건 
+				model.addAttribute("title","게시판 작성 성공");
+				model.addAttribute("msg","게시판 작성되었습니다");
+			}else {
+				model.addAttribute("title","게시판 작성 실패");
+				model.addAttribute("msg","에러찾으세요...");
+			}
+			model.addAttribute("loc","/board/list?reqPage=1");  //리스트로 이동
+			
 		}
-		model.addAttribute("loc","/board/list?reqPage=1");  //리스트로 이동
-		return "common/msg";
+				return "common/msg";
+		
 	}
 	
 	
