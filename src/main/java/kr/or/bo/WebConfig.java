d1a0c51a1f20178df25f48e1a242755fc217bdaf
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-08-31 17:32:28 +0900

    product webconfig 수정

--- src/main/java/kr/or/bo/WebConfig.java
+++ src/main/java/kr/or/bo/WebConfig.java
@@ -35,7 +35,7 @@ public class WebConfig implements WebMvcConfigurer{
 		.addPathPatterns("/member/list", "/member/changeLevel", "/member/checkedchangeLevel", "/member/find", "/member/findresult", "/member/levelSearchList");
 		
 		registry.addInterceptor(new BlackInterceptor())
-		.addPathPatterns("/wish/**","/mypage/**", "/product/**")
+		.addPathPatterns("/wish/**","/mypage/**")
 		.excludePathPatterns("/wish/list", "/wish/more");
 	}
 }
