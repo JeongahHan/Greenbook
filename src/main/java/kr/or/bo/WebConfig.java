cacfb99faf70328a98d1b2f86bffa0ceeaedfab2
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-09-01 09:48:35 +0900

    product 블랙이 접근차단

--- src/main/java/kr/or/bo/WebConfig.java
+++ src/main/java/kr/or/bo/WebConfig.java
@@ -36,7 +36,7 @@ public class WebConfig implements WebMvcConfigurer{
 		.addPathPatterns("/member/list", "/member/changeLevel", "/member/checkedchangeLevel", "/member/find", "/member/findresult", "/member/levelSearchList");
 		
 		registry.addInterceptor(new BlackInterceptor())
-		.addPathPatterns("/wish/**","/mypage/**")
+		.addPathPatterns("/wish/**","/mypage/**", "/product/board", "/product/productDetail")
 		.excludePathPatterns("/wish/list", "/wish/more");
 	}
 }
